757c62bb5d15c6c4c809d3ca80979136cb0c1227
Mohamed Essam <77198018+MohamedEssam9009@users.noreply.github.com>
2024-05-17 01:47:08 +0300

    put a home screen in separated widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 import 'constants.dart';
-import 'details_screen.dart';
-import 'widgets/category_title.dart';
-import 'widgets/food_card.dart';
+import 'home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,130 +28,6 @@ class MyApp extends StatelessWidget {
         ),
       ),
       home: const HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: Container(
-        padding: const EdgeInsets.all(10.0),
-        height: 80.0,
-        width: 80.0,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: kPrimaryColor.withOpacity(.26),
-        ),
-        child: Container(
-          padding: const EdgeInsets.all(20.0),
-          decoration: const BoxDecoration(
-            shape: BoxShape.circle,
-            color: kPrimaryColor,
-          ),
-          child: SvgPicture.asset('assets/icons/plus.svg'),
-        ),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(
-              right: 20.0,
-              top: 50.0,
-            ),
-            child: Align(
-              alignment: Alignment.topRight,
-              child: SvgPicture.asset(
-                'assets/icons/menu.svg',
-                height: 11.0,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Text(
-              'Simple way to find \nTasty food',
-              style: Theme.of(context).textTheme.headlineSmall,
-            ),
-          ),
-          const SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                CategoryTitle(title: 'All', active: true),
-                CategoryTitle(title: 'Italian', active: false),
-                CategoryTitle(title: 'Asian', active: false),
-                CategoryTitle(title: 'Chines', active: false),
-                CategoryTitle(title: 'Burgers', active: false),
-              ],
-            ),
-          ),
-          Container(
-            alignment: Alignment.centerLeft,
-            margin: const EdgeInsets.symmetric(
-              vertical: 20.0,
-              horizontal: 20.0,
-            ),
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20.0,
-              vertical: 15.0,
-            ),
-            height: 50.0,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20.0),
-              border: Border.all(color: kBoarderColor),
-            ),
-            child: SvgPicture.asset('assets/icons/search.svg'),
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                FoodCard(
-                  press: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const DetailsScreen(),
-                      ),
-                    );
-                  },
-                  title: 'Vegan salad bowl',
-                  ingredient: 'Tomato',
-                  image: 'assets/images/image_1.png',
-                  price: 20,
-                  calories: '420Kcal',
-                  description:
-                      'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
-                ),
-                FoodCard(
-                  press: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const DetailsScreen(),
-                      ),
-                    );
-                  },
-                  title: "Vegan salad bowl",
-                  ingredient: 'Tomato',
-                  image: "assets/images/image_2.png",
-                  price: 20,
-                  calories: "420Kcal",
-                  description:
-                      "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. ",
-                ),
-                const SizedBox(width: 20.0),
-              ],
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
